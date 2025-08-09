a9617c3094179b27e91020fa461e58c8dbd4ae2f
Brad Preston <bap5393@gmail.com>
2025-08-09 14:28:27 -0400

    :sparkles: setup tailwindcss / daisyui / icons (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-red-500">
+      Hello from the localhost!
+    </h1>
+
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>

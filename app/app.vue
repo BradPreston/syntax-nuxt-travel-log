ec9149f89732d0d79586b9082554705408b38ed6
Brad Preston <bap5393@gmail.com>
2025-08-09 14:48:08 -0400

    :sparkels: add landing page (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-red-500">
-      Hello from the localhost!
-    </h1>
-
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>

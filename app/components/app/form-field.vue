c8966369cc20bcbe0c7275da7c1470f95d482cf0
Brad Preston <bap5393@gmail.com>
2025-08-12 21:28:53 -0400

    :sparkles: add functionality to create new location (#60)

---
+++ app/components/app/form-field.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  name: string;
+  type?: "text" | "textarea" | "number";
+  error?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="type === 'number' ? 'input' : type || 'input'"
+      :name="props.name"
+      :type="type || 'text'"
+      class="w-full"
+      :class="{
+        'input-error': props.error,
+        'input': !type || type === 'text' || type === 'number',
+        'textarea': type === 'textarea',
+      }"
+      :disabled="disabled"
+    />
+    <p v-if="props.error" class="fieldset-label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
